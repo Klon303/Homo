f5e84e881ef96ee5d3fe0c37a5d5393bd2d0534a
Igor_Maer <igor.son303@yandex.by>
2025-08-03 01:08:29 +0300

    home 4 nemnogo

--- src/HW/4/Button.tsx
+++ src/HW/4/Button.tsx
@@ -1,11 +1,10 @@
 type ButtonPropsType = {
-  callBack: any // НУЖНО ПРОТИПИЗИРОВАТЬ
-  name: any // НУЖНО ПРОТИПИЗИРОВАТЬ
+ callBack: () => void;
+  name: string;
 };
 
 export const Button = (props: ButtonPropsType) => {
-  const callBackHandler = () => {
-    // НУЖНО ДОПИСАТЬ
+  const callBackHandler = () => {props.callBack();
   };
 
   return (
